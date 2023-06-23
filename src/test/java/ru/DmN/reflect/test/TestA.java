06285a138e727291b4a6033c3fa29ce4440376cd
DomamaN202 <vip.domaman@mail.ru>
2023-06-23 14:06:44 +0700

    =)

--- src/test/java/ru/DmN/reflect/test/TestA.java
+++ src/test/java/ru/DmN/reflect/test/TestA.java
@@ -2,18 +2,20 @@ package ru.DmN.reflect.test;
 
 import ru.DmN.reflect.UnsafeClassModifiersModification;
 
-public class Test {
+import java.lang.reflect.Modifier;
+
+public class TestA {
     public static void main(String[] args) {
-        var klassA = Test.class;
+        var klassA = TestA.class;
         System.out.println(klassA.getModifiers());
 
-        UnsafeClassModifiersModification.modify(klassA, Class.class);
+        UnsafeClassModifiersModification.modify(klassA, Modifier.PUBLIC | Modifier.FINAL);
         System.out.println(klassA.getModifiers());
 
         var klassB = Class.class;
         System.out.println(klassB.getModifiers());
-//
-        UnsafeClassModifiersModification.modify(klassB, Object.class);
+
+        UnsafeClassModifiersModification.modify(klassB, Modifier.PUBLIC);
         System.out.println(klassB.getModifiers());
     }
 }
